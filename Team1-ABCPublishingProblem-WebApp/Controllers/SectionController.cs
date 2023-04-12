dd27d04123aae0116ea98d22d36840f8c128eefd
Ntokozo Stevens <nkonzo144@gmail.com>
2023-04-12 11:59:45 +0200

    Added views and action methods for the views

---
+++ Team1-ABCPublishingProblem-WebApp/Controllers/SectionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team1_ABCPublishingProblem_WebApp.Controllers
+{
+    public class SectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("TableOfContents");
+        }
+
+        public IActionResult Book()
+        {
+            return View();
+        }
+
+		public IActionResult BookContent()
+		{
+			return View();
+		}
+
+        public IActionResult ChapterContent()
+        {
+			return View();
+		}
+	}
+}
